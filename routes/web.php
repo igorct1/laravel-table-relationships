512abd5306fa28e7dc0180825c105a50ad9bf615
Igor Tozetti <igorct77@gmail.com>
2022-07-31 18:02:02 -0300

    one-to-one

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,29 @@
 <?php
 
+use App\Models\{
+    User,
+    Preference,
+};
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/one-to-one', function(){
+    $user = User::with('preference')->first();
+    $data = [
+        'background_color' => '#fff',
+    ];
+    if($user->preference){
+        $user->preference->update($data);
+    }else {
+        $user->preference()->create($data);
+        // $preference = new Preference($data);
+        // $user->preference()->save($preference);
+    }
+    $user->refresh();
+    //deletar preferencia
+    $user->preference->delete(['background_color']);
+    dd($user->preference);
+});
+
 
 Route::get('/', function () {
     return view('welcome');
