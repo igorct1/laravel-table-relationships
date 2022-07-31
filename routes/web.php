5025584267dd1f5e41941caab58f22dc0f76f8e9
Igor Tozetti <igorct77@gmail.com>
2022-07-31 18:39:21 -0300

    one-to-many

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,38 @@
 use App\Models\{
     User,
     Preference,
+    Course,
+    Module,
 };
 use Illuminate\Support\Facades\Route;
+
+Route::get('/one-to-many', function () {
+    // $course = Course::create([
+    //     'name' => 'Curso de Laravel',
+    // ]);
+
+    $course = Course::with('modules.lessons')->first();
+    echo $course->name;
+    echo "<br>";
+    foreach ($course->modules as $module) {
+        echo "Module: {$module->name} <br>";
+        foreach($module->lessons as $lesson){
+            echo "lesson: {$lesson->name} <br>";
+            echo "lesson: {$lesson->video} <br>";
+        }
+    }
+    // $data = [
+    //     'name' => 'Módulo x1'
+    // ];
+    // $course->modules()->create($data);
+    // $mod1 = Module::find(1);
+    // $modules = $course->modules()->get();
+    // $mod1->lessons()->create([
+    //     'name' => 'Routing and Controller',
+    //     'video' => 'youtube.com/laradaily/routing-controller',
+    // ]);
+    // dd($mod1->lessons);
+});
 
 Route::get('/one-to-one', function(){
     $user = User::with('preference')->first();
@@ -19,11 +49,11 @@ Route::get('/one-to-one', function(){
         // $user->preference()->save($preference);
     }
     $user->refresh();
+
     //deletar preferencia
-    $user->preference->delete(['background_color']);
+    $user->preference->delete();
     dd($user->preference);
 });
-
 
 Route::get('/', function () {
     return view('welcome');
